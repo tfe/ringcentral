60a1a3970d608ace3a6f65f217ad59c75f3d506d
Dominic DeWolfe <d_dewolfe@yahoo.com>
2013-10-09 20:32:13 -0400

    reorder

--- lib/ringcentral.rb
+++ lib/ringcentral.rb
@@ -22,8 +22,8 @@ module RingCentral
 
       params = {
         :recipient => recipient,
-        :Coverpage => cover_page,
-        :Coverpagetext => cover_page_text,
+        :coverpage => cover_page,
+        :coverpagetext => cover_page_text,
         :resolution => resolution,
         :sendtime => send_time,
         :attachment => attachment
